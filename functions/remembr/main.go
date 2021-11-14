d696701fe9c3b1d63fbc9f223d4decb5d8ef0e5a
maxime.dubourg <maxime.dubourg@protonmail.com>
2021-11-15 00:05:30 +0100

    feat: reminders sending

--- functions/remembr/main.go
+++ functions/remembr/main.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"time"
 
@@ -18,15 +20,39 @@ import (
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
+type ApiReminder struct {
+	Id          int
+	Created_at  string
+	Object      string
+	Remind_date string
+	Sender_id   int
+}
+
 type Reminder struct {
 	Object     string
 	RemindDate string
 	SenderId   string
 }
 
-func insertReminder(reminder *Reminder) error {
-	endpoint := fmt.Sprintf("%s/rest/v1/Reminder", os.Getenv("SUPABASE_URL"))
+func GetPreparedHttpClient(
+	verb string,
+	body *bytes.Buffer,
+	queryParamsStr string,
+) (*http.Client, *http.Request) {
+	endpoint := fmt.Sprintf("%s/rest/v1/Reminder%s", os.Getenv("SUPABASE_URL"), queryParamsStr)
 
+	client := &http.Client{Timeout: time.Second * 10}
+	request, _ := http.NewRequest(verb, endpoint, body)
+
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("apikey", os.Getenv("SUPABASE_SECRET_KEY"))
+	request.Header.Set("Authorization", "Bearer "+os.Getenv("SUPABASE_SECRET_KEY"))
+	request.Header.Set("Prefer", "return=representation")
+
+	return client, request
+}
+
+func InsertReminder(reminder *Reminder) error {
 	payload, err := json.Marshal(&map[string]string{
 		"object":      reminder.Object,
 		"remind_date": reminder.RemindDate,
@@ -37,25 +63,64 @@ func insertReminder(reminder *Reminder) error {
 		log.Fatalf("error while encoding reminder. %s", err)
 	}
 
-	client := &http.Client{Timeout: time.Second * 10}
-	request, _ := http.NewRequest("POST", endpoint, bytes.NewBuffer(payload))
-
-	request.Header.Set("Content-Type", "application/json")
-	request.Header.Set("apikey", os.Getenv("SUPABASE_SECRET_KEY"))
-	request.Header.Set("Authorization", "Bearer "+os.Getenv("SUPABASE_SECRET_KEY"))
-	request.Header.Set("Prefer", "return=representation")
-
-	response, _ := client.Do(request)
+	client, request := GetPreparedHttpClient("POST", bytes.NewBuffer(payload), "")
+	response, err := client.Do(request)
 
 	if err != nil || response.StatusCode != 201 {
 		log.Fatalf("error while POSTing reminder. %s", err)
 	}
 
-	return nil
+	return err
 }
 
-func listPendingReminders() {
-	//endpoint := fmt.Sprintf("%s/rest/v1/Reminder", os.Getenv("SUPABASE_URL"))
+func ListPendingReminders() (*[]ApiReminder, error) {
+	tz, _ := time.LoadLocation("Europe/Paris")
+	now := time.Now().In(tz)
+
+	client, request := GetPreparedHttpClient(
+		"GET",
+		bytes.NewBuffer([]byte{}),
+		fmt.Sprintf("?remind_date=lte.%s", url.QueryEscape(now.Format(time.RFC3339))),
+	)
+	response, err := client.Do(request)
+
+	if err != nil || response.StatusCode != 200 {
+		log.Fatalf("error while listing pending reminders. %s", err)
+	}
+
+	defer response.Body.Close()
+	bodyBytes, _ := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var reminders []ApiReminder
+	err = json.Unmarshal(bodyBytes, &reminders)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &reminders, err
+}
+
+func DeletePassedReminders() error {
+	tz, _ := time.LoadLocation("Europe/Paris")
+	now := time.Now().In(tz)
+
+	client, request := GetPreparedHttpClient(
+		"DELETE",
+		bytes.NewBuffer([]byte{}),
+		fmt.Sprintf("?remind_date=lte.%s", url.QueryEscape(now.Format(time.RFC3339))),
+	)
+	response, err := client.Do(request)
+
+	if err != nil || response.StatusCode != 200 {
+		log.Fatalf("error while deleting passed reminders. %s", err)
+	}
+
+	return err
 }
 
 func main() {
@@ -87,34 +152,34 @@ func main() {
 		if err != nil {
 			b.Send(
 				m.Sender,
-				"Something wrong happened while handling your message, please try again later.",
+				"❌ Something wrong happened while handling your message, please try again later.",
 			)
 		}
 
 		// no date matching was found in the message
 		if r == nil || m.Text[0:r.Index] == "" {
-			b.Send(m.Sender, "Invalid task and/or date format.")
+			b.Send(m.Sender, "❌ Invalid task and/or date format.")
 			return
 		}
 
 		// given date in in the past
 		if r.Time.Before(now) {
-			b.Send(m.Sender, "Given date should be in the future.")
+			b.Send(m.Sender, "❌ Given date should be in the future.")
 			return
 		}
 
 		reminder := Reminder{
-			Object:     m.Text[0:r.Index],
+			Object:     m.Text[0 : r.Index-1],
 			RemindDate: r.Time.Format(time.RFC3339), // 2006-01-02T15:04:05Z07:00
 			SenderId:   fmt.Sprint(m.Sender.ID),
 		}
 
 		// inserting reminders in database
-		err = insertReminder(&reminder)
+		err = InsertReminder(&reminder)
 		if err != nil {
 			b.Send(
 				m.Sender,
-				"Something wrong happened while handling your message, please try again later.",
+				"❌ Something wrong happened while handling your message, please try again later.",
 			)
 		}
 
@@ -127,10 +192,27 @@ func main() {
 
 	lambda.Start(func(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 		if req.HTTPMethod == "GET" {
-			// reminders :=
+			reminders, err := ListPendingReminders()
+
+			if err == nil {
+				for _, reminder := range *reminders {
+					b.Send(&tb.User{ID: reminder.Sender_id}, fmt.Sprintf("📣 %s", reminder.Object))
+
+					// TODO: Add snooze feature
+				}
+			}
+
+			err = DeletePassedReminders()
+
+			if err == nil {
+				return &events.APIGatewayProxyResponse{
+					StatusCode: 200,
+				}, nil
+			}
 
 			return &events.APIGatewayProxyResponse{
-				StatusCode: 200,
+				StatusCode: 500,
+				Body:       err.Error(),
 			}, nil
 		} else if req.HTTPMethod == "POST" {
 			var u tb.Update
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/aws/aws-lambda-go v1.27.0
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/olebedev/when v0.0.0-20190311101825-c3b538a97254 // indirect
+	github.com/ugorji/go/codec v1.2.6 // indirect
 	gopkg.in/tucnak/telebot.v2 v2.4.1 // indirect
 )
